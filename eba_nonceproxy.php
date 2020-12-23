46d728594152da794dd4994b316db1fe91ba531d
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-23 11:13:16 +0300

    Update eba_nonceproxy.php

--- eba_nonceproxy.php
+++ eba_nonceproxy.php
@@ -4,4 +4,4 @@
 // Sunucuma eklediğim bu dosya ile header eklemiş oluyoruz ve JS'den kullanabiliyoruz.
 header("Access-Control-Allow-Origin: *");
 if(ctype_xdigit($_GET['nonce']))
-        echo file_get_contents("https://uygulama-ebaders.eba.gov.tr/FrontEndService/livelesson/nonce/".$_GET['nonce']);
+        echo file_get_contents("https://uygulama.sebitvcloud.com/VCloudFrontEndService/livelesson/nonce/".$_GET['nonce']);
